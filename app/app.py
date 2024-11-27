9d31f93adfa4216e64910f5c1cc2e27fb67d9393
MarcinZarkowski <147103447+MarcinZarkowski@users.noreply.github.com>
2024-11-26 23:47:35 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,13 +5,12 @@ from .models import SessionLocal, Chat
 from datetime import datetime, timedelta, timezone
 import requests
 from .get_stock_data import *
-from .Rag_handler import add_articles, add_tables, add_data, use_RAG_pipeline, delete_data, load, refresh_retriever, show_vstore, is_rag_usefull, extract_keywords, vector_data_base
+from .Rag_handler import add_articles, add_tables, add_data, use_RAG_pipeline, delete_data, refresh_retriever, is_rag_usefull
 import pandas as pd
-import nltk
+
 import os
 
 from dotenv import load_dotenv
-
 
 
 load_dotenv()
@@ -47,12 +46,13 @@ async def log_request(request: Request, call_next):
 
 @app.post("/start-chat")
 async def start_conversation(body: dict = Body(...), db: Session = Depends(get_db)):  
-    global vector_data_base
+    #global vector_data_base
     key=body.get("key")
+
     if key!= os.getenv("KEY"):
         return {"message": "Invalid key"}
     
-    show_vstore(vector_data_base)
+    #show_vstore(vector_data_base)
     stock = body.get("stock")
     db_chat = db.query(Chat).filter(Chat.title == stock).first()
 
@@ -64,61 +64,72 @@ async def start_conversation(body: dict = Body(...), db: Session = Depends(get_d
 
         # Check if the chat is older than 12 hours
         if datetime.now(timezone.utc) - created_at > timedelta(hours=12): 
-            db.delete(db_chat)
-            db.commit()
+            ids=json.loads(db_chat.ids)
             print("TOO OLD, Making new")
-            vector_data_base=await delete_data(stock, vector_data_base)
-            
+            await delete_data(ids) #wait until data is deleted
+
+            db.delete(db_chat) #delete chat from db
+            db.commit()
+            print("deleted")
 
             #get new data
             context = await get_stock_context(stock)
             
-            vector_data_base= await add_stock_context(stock, context, db) 
-            refresh_retriever(vector_data_base)
-            show_vstore(vector_data_base)
-            
+            await add_stock_context(stock, context, db) #wait to refresh retreiver until data is added
+            refresh_retriever()
+            #show_vstore(vector_data_base)
+        else:
+            print("NOT TOO OLD")
+    
     else:
         context = await get_stock_context(stock)
-        vector_data_base= await add_stock_context(stock, context, db) 
-        refresh_retriever(vector_data_base)
-        show_vstore(vector_data_base)
+        await add_stock_context(stock, context, db) 
+        refresh_retriever()
+        #show_vstore(vector_data_base)
        
     
         
     
      
-
+#creates SQlite instance of a conversation about a stock and stores its context, Document ids to then easily delete from vector databse and timestamp 
+#to keep track of how old the context is
 async def add_stock_context(stock, context, db: Session):
-    global vector_data_base
+    #global vector_data_base
+    
     try:
         db_chat = db.query(Chat).filter(Chat.title == stock).first()
-        if not db_chat:
-            
+        if not db_chat:#check one more time if chat exists
+            ids=[]
          
             for key, value in context.items():
                 if key == "news":
-                    vector_data_base = await add_articles(stock, value, vector_data_base)
+                    ids_just_added = await add_articles(stock, value)
+                    ids.extend(ids_just_added)
                 elif key == "calendar" or key == "analyst_price_targets":
-                    vector_data_base = await add_data(stock, value, vector_data_base)
+                    ids_just_added= await add_data(stock, value )
+                    ids.extend(ids_just_added)
                 else:
-                    vector_data_base = await add_tables(stock, key, value, vector_data_base)
+                    ids_just_added = await add_tables(stock, key, value)
+                    ids.extend(ids_just_added)
 
-            save_directory="app/vector_database"
-            vector_data_base.save_local(save_directory)
+            '''save_directory="app/vector_database"
+            vector_data_base.save_local(save_directory)'''
+
 
             #only save to sqlite once vectordb processes completed
-            context_json = json.dumps(context, default=str)  
-            db_chat = Chat(title=stock, context=context_json)
+            #context_json = json.dumps(context, default=str)  
+            ids=json.dumps(ids)
+            db_chat = Chat(title=stock, ids=ids)#context=context_json
             db.add(db_chat)
             db.commit()
             db.refresh(db_chat)
     
         
-        return vector_data_base
+        return 
 
     except Exception as e:
         print(f"Error in add_stock_context: {e}")
-        return vector_data_base
+        return 
 '''      
 #@app.post("/ask")
 async def ask(body: dict=Body(...), db: Session = Depends(get_db)):
@@ -146,8 +157,8 @@ async def ask(body: dict=Body(...), db: Session = Depends(get_db)):
 
 @app.websocket('/ask')
 async def send_chat(websocket: WebSocket):
-    global vector_data_base
-    show_vstore(vector_data_base)
+    #global vector_data_base
+    #show_vstore(vector_data_base)
     await websocket.accept()
    
     while True:
@@ -159,26 +170,23 @@ async def send_chat(websocket: WebSocket):
         question = parsed_data.get("query")
         ticker = parsed_data.get("ticker")
         
-        should_we_ask=False
-        for word in extract_keywords(question):
-            if is_rag_usefull(word, vector_data_base):
-                should_we_ask=True
-                break
-
-        if not should_we_ask:
+        
+        usefull, context= is_rag_usefull(question)
+        if not usefull:
             await websocket.send_text(json.dumps({"event_type": "bad_request"}))
             await websocket.close()
             return
-
-        should_we_ask=is_rag_usefull(question, vector_data_base)
-        
+             
+        print("got after check")
         
         question= question + f"(This prompt is about {ticker})"
-       
 
-        async for event in use_RAG_pipeline(question):
-           
-            if event["event_type"] == "done":
+        async for event in use_RAG_pipeline(question, context):
+            if event["event_type"]=="error":
+                await websocket.send_text(json.dumps({"event_type": "bad_request"}))
+                await websocket.close()
+                return
+            elif event["event_type"] == "done":
                 await websocket.close()
                 return 
             else:
